19183a6ea429957f785732ddaac2b6a0cc48947d
miso0001 <46448554+miso0001@users.noreply.github.com>
2019-02-08 19:40:52 +0100

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Michal Severa on 08/02/2019.
+//  Copyright © 2019 Michal Severa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
